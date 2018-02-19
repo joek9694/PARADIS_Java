240e6173319fd441d39b72ed6a07972f8ab5e047
joek9694 <johan.eklundh@gmail.com>
2018-02-19 21:12:03 +0100

    update2

--- src/assign1/Factorizer.java
+++ src/assign1/Factorizer.java
@@ -3,6 +3,7 @@ package assign1;
 public class Factorizer {
 	//just an update
 	//another update to see if warnings spread
+	//noSpread... will it warn again?
 	
 	public static void main(String[] args) {
 		
